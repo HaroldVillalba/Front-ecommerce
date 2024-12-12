42c510633297222cbd48db02fdef592812ae7c43
Harold Villalba <haroldllalba101806@gmail.com>
2024-12-12 14:56:42 -0500

    modificacion del header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -8,12 +8,15 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  // Verificar usuario al cambiar de ruta
   useEffect(() => {
     const usuarioGuardado = localStorage.getItem("usuario");
     if (usuarioGuardado) {
       setUsuario(JSON.parse(usuarioGuardado));
+    } else {
+      setUsuario(null);
     }
-  }, []);
+  }, [location.pathname]); // Se ejecuta cada vez que cambia la ruta
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -23,15 +26,16 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-gray-800 text-white py-4">
+    <header className="bg-blue-300 text-gray-800 py-6">
       <nav className="container mx-auto flex justify-between items-center">
-        <h1 className="text-lg font-bold">Mi Proyecto</h1>
+        <h1 className="text-lg font-bold">Aleluya</h1>
         <div className="flex space-x-4 items-center">
-          {location.pathname === "/productos" && usuario ? ( // Mostrar solo en /productos
+          {location.pathname === "/productos" && usuario ? (
+            // Mostrar solo en /productos
             <>
               <div className="flex items-center space-x-2">
-                <FontAwesomeIcon icon={faUser} className="text-gray-300" />
-                <span className="text-gray-300">{usuario.nombre}</span>
+                <FontAwesomeIcon icon={faUser} className="text-gray-800" />
+                <span className="text-gray-600">{usuario.nombre}</span>
               </div>
               <button
                 onClick={handleLogout}
@@ -41,7 +45,7 @@ const Header = () => {
               </button>
             </>
           ) : (
-            <Link to="/login" className="hover:text-gray-300">
+            <Link to="/login" className="hover:text-gray-800">
               Iniciar Sesión
             </Link>
           )}
