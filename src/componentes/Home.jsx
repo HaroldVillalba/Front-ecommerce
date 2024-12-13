3e2ae0f60a1d0b759e163ed9c2801dd8df88b792
Faveste Valencia <Favian.saa@correounivalle.edu.co>
2024-12-12 19:39:57 -0500

    Solucion Error

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -37,27 +37,26 @@ const Home = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-6">
         {recipes.map((Recipe) => (
           <button
-          key={Recipe._id}
-          onClick={() => {/* Agrega aquí la lógica de clic si es necesario */}}
-          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-        >
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {Recipe.title}
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {Recipe.category} 
-          </p>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {Recipe.ingredients} 
-          </p>
-          <h5 className="font-normal text-gray-700 dark:text-gray-400">
-            {Recipe.intructions}
-          </h5>
-          <small className="text-gray-500 dark:text-gray-400">
-            Fecha de creación: {new Date(producto.fechaCreacion).toLocaleDateString()}
-          </small>
-        </button>
-        
+            key={Recipe._id}
+            onClick={() => {/* Agrega aquí la lógica de clic si es necesario */}}
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {Recipe.title}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {Recipe.category} 
+            </p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {Recipe.ingredients} 
+            </p>
+            <h5 className="font-normal text-gray-700 dark:text-gray-400">
+              {Recipe.intructions}
+            </h5>
+            <small className="text-gray-500 dark:text-gray-400">
+              Fecha de creación: {new Date(Recipe.fechaCreacion).toLocaleDateString()}
+            </small>
+          </button>
         ))}
       </div>
     </div>
